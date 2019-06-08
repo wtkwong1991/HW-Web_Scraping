b5df5588f74617bff982e476fb80b84eb13017a9
wtkwong1991 <47372271+wtkwong1991@users.noreply.github.com>
2019-06-08 11:15:09 -0500

    Add files via upload

--- HW12/app.py
+++ HW12/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect
-from flask_pymongo import PyMongo
+import PyMongo
+
 import scrape_mars
 
 # Create an instance of Flask
--- HW12/scrape_mars.py
+++ HW12/scrape_mars.py
@@ -1,5 +1,6 @@
 from splinter import Browser
-from bs4 import BeautifulSoup as bs
+from bs4 import BeautifulSoup
+
 import time
 import requests
 import pymongo
